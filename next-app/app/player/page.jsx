15302c608c186d30cb85445eda4add8d7a6bc866
peachonka <okneverak@gmail.com>
2024-10-10 20:42:04 +0300

    почти готова страница игрока

--- next-app/app/player/page.jsx
+++ next-app/app/player/page.jsx
@@ -1,5 +1,15 @@
-import "../../app/team/tbody.css"
 import Active from "@/components/titles/active-player"
+import  '../../styles/tbody.css'
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table.tsx"
+import StatTitle from "@/components/titles/statistics"
+
 
 const backgroundImage = 'url(https://avatars.mds.yandex.net/get-shedevrum/5570741/img_b5b38b9886fd11efb424c6eb7d220bbe/orig)'
 const biograhpy =`Лукас Томпсон начал свою профессиональную карьеру в юношеской академии "Манчестер Юнайтед" в возрасте 8 лет. Он быстро продвигался по служебной лестнице, впечатляя тренеров своей скоростью, ловкостью и мастерством на добивании. В 2013 году он дебютировал на профессиональном уровне за "Манчестер Юнайтед" в матче Лиги чемпионов против мадридского "Реала".
@@ -14,30 +24,64 @@ const items = [
 ]
 
 export default function Component() {
+  const tableData = Array.from({ length: 7 }, (_, rowIndex) =>
+    Array.from({ length: 7 }, (_, colIndex) => `${rowIndex + 1}-${colIndex + 1}`)
+  )
   return (
-    <div className="h-full bg-white text-black p-4">
-        <div className="flex items-start space-x-4">
-          <div className='preview' key={items.id} style={{backgroundImage: backgroundImage}}></div>
-          <div className="flex-1">
-            <div className="mb-4">
-                <h2 className="text-4xl font-bold">Иван Иванов</h2>
-                <Active/>
-            </div>
-            <p className="mt-1 text-xl">Нападающий</p>
-          </div>
-        </div>
-        <div className=" flex mt-4 bg-black text-white rounded-2xl p-2 max-w-max">
-        {items.map((item, index) => (
-          <div className='col m-2' key={index}>
-            <div className="text-base text-center m-2">
-                {item.title}
-            </div>
-            <div className="text-lg font-bold text-center m-2">
-                {item.value}
+    <div className="h-full bg-white flex flex-wrap text-black p-4">
+      <div className="flex flex-col mb-8">
+          <div className="flex flex-wrap space-x-4">
+            <div className='preview mb-4' key={items.id} style={{backgroundImage: backgroundImage}}></div>
+            <div className="flex-1">
+              <div className="mb-4">
+                  <h2 className="text-4xl">Иван Иванов</h2>
+                  <Active/>
+              </div>
+              <p className="mt-1 text-xl">Нападающий</p>
             </div>
           </div>
-        ))}
-        </div>
+          <div className=" flex mt-4 bg-black text-white rounded-2xl p-2 max-w-max">
+          {items.map((item, index) => (
+            <div className='col m-2' key={index}>
+              <div className="text-base text-center m-2">
+                  {item.title}
+              </div>
+              <div className="text-lg font-bold text-center m-2">
+                  {item.value}
+              </div>
+            </div>
+          ))}
+          </div>
+      </div>
+      <div className=" flex flex-col max-w-max text-gray-500 px-6">
+        <h2 className="text-4xl">Биография</h2>
+        <p className="my-2 max-w-3xl text-justify indent-4">{biograhpy}</p>
+      </div>
+      <div className="container flex flex-col mx-auto  mt-8">
+        <StatTitle/>
+        <Table className="my-4 rounded-lg overflow-hidden border-2 border-black">
+          <TableHeader className="">
+            <TableRow className="">
+              {["", "", "И", "Г", "ПАС.", "ЖК", "КК"].map((item, index) => (
+                <TableHead key={index} className="p-4 text-center border-2 border-black">
+                  {item}
+                </TableHead>
+              ))}
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+          {tableData.map((row, rowIndex) => (
+            <TableRow key={rowIndex}>
+              {row.map((cell, cellIndex) => (
+                <TableCell key={cellIndex} className="text-center border-2 border-black">
+                  {cell}
+                </TableCell>
+              ))}
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </div>
     </div>
   )
 }
